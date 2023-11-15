8b8d7e2e39a536f852963363d46a5f98db1298a0
Sok Sothy <122692075+Soksothy@users.noreply.github.com>
2023-11-15 14:22:20 +0700

    Delete project3 directory

--- project3/src/main/java/com/example/project3/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.project3;
-
-public class Main {
-    public static void main(String[] args){
-        Username_Password usernamePassword = new Username_Password();
-
-        Login loginPage = new Login(usernamePassword.getLoginInfo());
-    }
-}
